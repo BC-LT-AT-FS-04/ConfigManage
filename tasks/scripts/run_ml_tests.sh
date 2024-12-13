a4099734804e0154a9e37b3ed3789895d1bf8b63
Joselian2204 <61953806+Joselian2204@users.noreply.github.com>
2024-12-13 16:21:10 -0400

    Add UA testing in MLService (#42)
    * Create the Test Script
    
    * Create the UAT Deployment Playbook deploy_converter_uat.yml
    
    * Add test video file
    
    * Integration into the playbook.yml
    
    * Comment test_video_to_video endpoint
    
    * Delete extra delimiter
    
    * Update test file path
    
    * Change Trutgy value
    
    * Add step to install community.docker
    
    * Add block to group related tasks
    
    * Change the execution on the runner. Add step Run UA tests
    
    * Delete deploy_converter_uat.yml file
    
    * Upload files for testing and update run_converter_tests.sh
    
    * Delete double testing for audio-to-audio endpoint
    
    * Modify deploy_converter.yml to test the script
    
    * Fix: Set executable permissions for run_converter_tests.sh
    
    * Restoring deploy_converter.yml file
    
    * Set executable permissions for UAT
    
    * URL for UAT changed
    
    * URL Modified and test assets removed
    
    * UAT modified for POST methods
    
    * URL for production server changed
    
    * URL security changed
    
    * API for endpoint modified
    
    * Try for recognizer
    
    * Using ML service url
    
    * Test for face recognition
    
    * Downloading temp file for face recognition
    
    * Temp file renamed and formated to .jpg
    
    * change desired name for image reference
    
    * Temp File changed to variable
    
    * Quotation added
    
    * model type and word changed for face recognition
    
    * Face recognition UAT skipped
    
    * Binary files removed
    
    ---------
    
    Co-authored-by: Jessica Orihuela Rojas <jessica.orihuela.rojas@gmail.com>
    Co-authored-by: Caleb Espinoza G. <caleb.espinoza@outlook.com>

---
+++ tasks/scripts/run_ml_tests.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# Script for User Acceptance Testing of MLService
+
+#Image configuration variables
+drive_file_id="1TjCRJQr3_102Y2OmvrDiljOATqDc68Hd"
+temp_file=$(mktemp --suffix=".jpg")
+desired_name="reference_image.jpg"
+
+# Download 
+echo "Downloadign file from Google Drive..."
+curl -L "https://drive.google.com/uc?export=download&id=$drive_file_id" -o "$temp_file"
+if [ $? -ne 0 ]; then
+    echo "Download error"
+    exit 1
+fi
+
+# List of endpoints
+endpoints=(
+    "http://mlservice.at04.devops.jala.university/api/recognition"
+    "http://mlservice.at04.devops.jala.university/api/face_recognition"
+)
+
+# Payload for the recognition endpoint
+payload_recognition='{
+    "zip_url": "http://10.27.5.151:9090//api/download-frames/ac87079166408a172599b7acdd151fa3708a684f15e9592a50a835b50743313a.zip",
+    "model_type": "object",
+    "confidence_threshold": 0.5,
+    "word": "dog"
+}'
+
+# Data for the face_recognition endpoint
+zip_url="http://10.27.5.151:9090//api/download-frames/556f030a81257cbfeacf800c2a1443a1dddea8dcae5ce58eb15dde6d83027663.zip"
+model_type="object"
+confidence_threshold=0.1
+word="face recognition"
+
+# Variable to track overall status
+overall_status=0
+
+# Iterate over endpoints and test them
+for endpoint in "${endpoints[@]}"; do
+    echo "Testing: $endpoint (POST)"
+
+    # Prepare the common curl command structure
+    if [[ "$endpoint" == *"/recognition" ]]; then
+        # Send JSON payload for /recognition endpoint
+        response_code=$(curl -s -o /dev/null -w "%{http_code}" \
+            -X POST "$endpoint" \
+            -H "Content-Type: application/json" \
+            -d "$payload_recognition")
+    else
+        # Send form-data for /face_recognition endpoint
+        #TODO: Wait for refactoring of the endpoint in MLService
+        echo "Face_recognition Skipped"
+        # response_code=$(curl -s -o /dev/null -w "%{http_code}" \
+        #     -X POST "$endpoint" \
+        #     -F "zip_url=$zip_url" \
+        #     -F "model_type=$model_type" \
+        #     -F "confidence_threshold=$confidence_threshold" \
+        #     -F "word=$word" \
+        #     -F "image_file_reference=@${temp_file}")
+    fi
+
+    # Check the response code
+    if [ "$response_code" -eq 200 ]; then
+        echo "$endpoint responded with HTTP 200."
+    else
+        echo "$endpoint failed with HTTP code $response_code."
+        overall_status=1
+    fi
+done
+
+# Exit with the overall status
+if [ "$overall_status" -eq 0 ]; then
+    echo "All acceptance tests passed successfully."
+else
+    echo "Some acceptance tests failed."
+fi
+
+exit $overall_status
+
